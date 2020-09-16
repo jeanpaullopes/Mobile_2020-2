42ac4e0037cd066b96c9eb76800cba5f3fe041ac
Prof Jean Paul <jean.paul@uniritter.edu.br>
2020-09-16 20:48:31 -0300

    Arquivos da aula 16 de setembro 2020

--- app/src/main/java/br/edu/uniritter/mobile/minhaprimeiraapp/MainActivity.java
+++ app/src/main/java/br/edu/uniritter/mobile/minhaprimeiraapp/MainActivity.java
@@ -13,22 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
         Button button = (Button) findViewById(R.id.button2);
+        button.setText("de volta ao jogo");
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), SegundaActivity.class);
                 startActivity(intent);
             }
         });
-  */
+
+
     }
-/*
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+       /*
+        Button button = (Button) findViewById(R.id.button2);
+        button.setText("de volta ao jogo");
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SegundaActivity.class);
+                startActivity(intent);
+            }
+        });
+        */
+    }
+
     public void botaoClick(View view) {
+        Button button = (Button) findViewById(R.id.button2);
+        button.setText("clicado");
         Intent intent = new Intent(this, SegundaActivity.class);
         startActivity(intent);
 
     }
-    */
+
 
 }
